6df38130c24fac55a369b6df61c03260ab93e165
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2023-09-14 00:37:05 -0300

    add: slider na home

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+var swiper = new Swiper(".slider-content", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
