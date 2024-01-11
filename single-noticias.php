14d9b270bd76f1484e100eb7d421bc941621a081
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2024-01-11 15:51:03 -0300

    add: Criação do single-noticias

---
+++ single-noticias.php
@@ -0,0 +1,41 @@
+    <?php
+    // add header
+    get_header();
+    if (have_posts()) : while (have_posts()) : the_post();
+            // $background = get_custom_meta($post->ID, "background_programas");
+    ?>
+
+            <main>
+                <?php if (empty($background)) : ?>
+                    <section id="intro-sub">
+                    <?php else : ?>
+                        <section id="intro-sub" style="background: url(<?= $background ?>) no-repeat center center/cover">
+                        <?php endif; ?>
+                        <div class="container">
+                            <div class="col-12 text-center">
+                                <h1><?php the_title(); ?></h1>
+
+                                <p>
+                                    <?php the_excerpt(); ?>
+                                </p>
+                            </div>
+                        </div>
+                        </section>
+
+                        <section id="programa-estruturado-data">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <p><?php the_content(); ?></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+            </main>
+
+    <?php
+        endwhile;
+    endif;
+    // add header
+    get_footer();
+    ?>
