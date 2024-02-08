b8fd9c4522a8fb0a3919433041a0e4fa5eaf681d
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2024-02-08 00:48:55 -0300

    add: adição da id post-container para estilização do parágrafo

--- single-noticias.php
+++ single-noticias.php
@@ -24,10 +24,10 @@
                         </div>
                         </section>
 
-                        <section id="programa-estruturado-data">
+                        <section id="post-content">
                             <div class="container">
                                 <div class="row">
-                                    <div class="col-12">
+                                    <div class="col-12 post-text">
                                         <p><?php the_content(); ?></p>
                                     </div>
                                 </div>
